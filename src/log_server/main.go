5c34c9caa4b724fa5f956ac22f419cde67da5588
zkb <zhengkunbin@onemt.com.cn>
2018-07-09 09:35:20 +0800

    日志服务

--- src/log_server/main.go
+++ src/log_server/main.go
@@ -2,20 +2,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"frameworks/util"
 	"net/http"
 	"sync"
 )
 
-// 我也改  
+var addr = flag.String("addr", "localhost:5001", "http server address")
+
 // 开启http服务
 func StartHttp(addr string /*, ch chan def.MsgChan*/) {
 	fmt.Println("StartHttp!!", addr)
-	//	heartUrl = strHeart
-	//	msgChan = ch
-	// 第一个参数为客户的发起http请求时的接口名，第二个参数是一个func，负责处理这个请求。
-	//	http.HandleFunc("/", heartBeat)
+
 	http.HandleFunc("/", actionHandle)
 
 	// 服务器要监听的主机地址和端口
@@ -26,26 +25,15 @@ func StartHttp(addr string /*, ch chan def.MsgChan*/) {
 }
 
 func actionHandle(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("Action Handle!")
-
-	//	ret := define.HttpReturn{1, "UnKnown Error"} // 返
-	// read body
 	data := make([]byte, 1024*1024)
 	n, _ := req.Body.Read(data)
-	//	if n >= 1024 {
-	//		fmt.Fprintf(w, ret.ToJson()) // 返回回去
-	//		return
-	//	}
 	// 最后err会等于EOF
 	cBody := data[:n]
 	strBody := string(cBody)
 	util.RunRecord("%s", strBody)
-	fmt.Println(strBody)
 
 	// 解析action
 	req.ParseForm()
-	action, found := req.Form["action"]
-	fmt.Println(action, found)
 
 	// 消息解析和处理并返回
 	resStr := "ok"
@@ -53,7 +41,7 @@ func actionHandle(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	go StartHttp("127.0.0.1:5001")
+	go StartHttp(*addr)
 
 	var wg sync.WaitGroup
 	wg.Add(1)
